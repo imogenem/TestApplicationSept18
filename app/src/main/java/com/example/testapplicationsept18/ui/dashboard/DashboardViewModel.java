012e121d4631ce7bcad2078efb12f53c3a64e614
Jeffrey Kwok <47469240+jkwok2@users.noreply.github.com>
2020-09-18 16:33:11 -0700

    test commit

--- app/src/main/java/com/example/testapplicationsept18/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/example/testapplicationsept18/ui/dashboard/DashboardViewModel.java
@@ -10,7 +10,7 @@ public class DashboardViewModel extends ViewModel {
 
     public DashboardViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("My name is Jeff");
     }
 
     public LiveData<String> getText() {
